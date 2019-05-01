f0838ff6d6f1993fbad54b9a2b7aef436a76ac4b
amirnejati <g2kfhu39pb>
2019-05-01 21:21:55 +0430

    prepare for heroku

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def main_route():
     return '<p><b>Salaam</b><br>private_ip: %s<br>public_ip: %s</p>' % \
-           (request.headers.get('X-Forwarded-For'), request.remote_addr)
+           (request.remote_addr, request.headers.get('X-Forwarded-For'))
 
 
 if __name__ == '__main__':
