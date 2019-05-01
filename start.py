298ed1eaeacbef70056700deed285c17f08af215
amirnejati <g2kfhu39pb>
2019-05-01 21:09:32 +0430

    prepare for heroku

--- start.py
+++ start.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_route():
-    return 'Salaam. Your IP is: %s' % request.remote_addr
+    responses = []
+    responses.extend(['X-Forwarded-For', request.headers.get('X-Forwarded-For')])
+    responses.extend(['HTTP_X_FORWARDED_FOR', request.headers.get('HTTP_X_FORWARDED_FOR')])
+    responses.extend(['REMOTE_ADDR', request.headers.get('REMOTE_ADDR')])
+    responses.extend(['X-Real-IP', request.headers.get('X-Real-IP')])
+    responses.extend(['X-Real-IP', request.headers.get('X-Real-IP')])
+    responses.extend(['FLASK.remote_addr', request.remote_addr])
+    return 'Salaam. Your IP is: \r\n%s' % ', '.join(str(i) for i in responses)
 
 
 if __name__ == '__main__':
