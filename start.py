957f9a131285e4adeef3b4d3ef9e456292912e91
Amir H. Nejati <amirnejati@users.noreply.github.com>
2019-05-01 17:33:53 +0430

    Update start.py
    print ip by flask

--- start.py
+++ start.py
@@ -1,4 +1,13 @@
-﻿import os
+from flask import Flask, request
 
-print('commit 1')
-print('B2')
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_route():
+    return 'Salaam. Your IP is: %s' % request.remote_addr
+
+
+if __name__ == '__main__':
+    port_number = 8080
+    app.run('0.0.0.0', port_number, threaded=True, debug=__debug__)
